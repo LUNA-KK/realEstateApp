3029f59617c4c672502f94e584dbd12cb1b36d17
lunak <03somin0324@gmail.com>
2025-04-01 16:57:00 +0900

    feat: 이미지 미리보기 기능 추가

--- src/app/createHouse/pages/ImageForm/index.tsx
+++ src/app/createHouse/pages/ImageForm/index.tsx
@@ -1,13 +1,48 @@
 import Button from "@/components/Button";
 import styles from "../index.module.css";
 import { useStep } from "../../useStep";
+import { useRef, useState } from "react";
 
 export default function ImageForm() {
   const { nextStep } = useStep();
+  const ref = useRef<HTMLInputElement>(null);
+  const openFile = () => {
+    ref.current?.click();
+  };
+  const [url, setUrl] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const imageUrl = e.target?.result;
+      setUrl(imageUrl as string);
+    };
+    reader.readAsDataURL(file);
+    e.target.value = "";
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <h1>사진을 업로드 해주세요.</h1>
+        매물 사진
+        <button className={styles.button} onClick={openFile}>
+          사진 추가
+        </button>
+        <input
+          className={styles.file}
+          type="file"
+          ref={ref}
+          accept="image/*"
+          onChange={handleChange}
+        />
+        {url && (
+          <img
+            src={url}
+            style={{ width: "100%", height: "300px", objectFit: "contain" }}
+          />
+        )}
       </div>
       <Button onClick={nextStep}>다음</Button>
     </div>
