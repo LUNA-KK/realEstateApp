48210e9608e764caf00085fd54e2814befa7c269
lunak <03somin0324@gmail.com>
2025-05-24 19:34:55 +0900

    feat: 지역 필터링 기능 추가 및 API 호출 로직 구현

--- src/app/filterPage/page.tsx
+++ src/app/filterPage/page.tsx
@@ -3,10 +3,189 @@
 import Button from "@/components/Button";
 import styles from "./page.module.css";
 import Slider from "@mui/material/Slider";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { authFetch } from "../util/authFetch";
+import { create } from "zustand";
+import useRegionCode from "../store/useRegionCode";
+
+interface RegionProps {
+  title: string;
+}
+
+interface AddressResposne {
+  cortar_no: string;
+  center_lat: number;
+  center_lon: number;
+  cortar_name: string;
+  cortar_type: string;
+}
+
+type Response = AddressResposne[];
+
+const RegionItem = ({
+  code,
+  name,
+  onClick,
+  isClicked = false,
+}: {
+  code: string;
+  name: string;
+  onClick: (code: string) => void;
+  isClicked?: boolean;
+}) => {
+  const handleClick = () => {
+    onClick(code);
+  };
+
+  return (
+    <div
+      className={styles["region-item"]}
+      onClick={handleClick}
+      style={{
+        backgroundColor: isClicked ? "#b0e4ff" : "transparent",
+      }}
+    >
+      {name}
+    </div>
+  );
+};
+
+const getNextCode = async (code: string) => {
+  const resposne = await authFetch({
+    url: `/api/house-board/addrCode?code=${code}`,
+  });
+  if (!resposne.ok) {
+    throw new Error("Failed to fetch next code");
+  }
+  const data: Response = await resposne.json();
+  return data;
+};
+
+interface SelectRegion {
+  selected: {
+    city: string;
+    dvsn: string;
+    sect: string;
+  };
+  setSelected: (key: string, selected: string) => void;
+}
+const useSelectRegion = create<SelectRegion>((set) => ({
+  selected: {
+    city: "0000000000",
+    dvsn: "",
+    sect: "",
+  },
+  setSelected: (key: string, selected: string) =>
+    set((state) => ({
+      selected: {
+        ...state.selected,
+        [key]: selected,
+      },
+    })),
+}));
+
+interface RegionStore {
+  cityList: Response;
+  dvsnList: Response;
+  sectList: Response;
+  setCityList: (list: Response) => void;
+  setDvsnList: (list: Response) => void;
+  setSectList: (list: Response) => void;
+}
+
+const useRegionStore = create<RegionStore>((set) => ({
+  cityList: [],
+  dvsnList: [],
+  sectList: [],
+  setCityList: (list: Response) => set({ cityList: list }),
+  setDvsnList: (list: Response) => set({ dvsnList: list }),
+  setSectList: (list: Response) => set({ sectList: list }),
+}));
+
+const FilterRegion = ({ title }: RegionProps) => {
+  const {
+    cityList,
+    setCityList,
+    dvsnList,
+    setDvsnList,
+    sectList,
+    setSectList,
+  } = useRegionStore();
+  const { setCode } = useRegionCode();
+  const { selected, setSelected } = useSelectRegion();
+
+  const cityClick = async (code: string) => {
+    const nextList = await getNextCode(code);
+    setSelected("city", code);
+    setDvsnList(nextList);
+    setSectList([]);
+  };
+
+  const dvsnClick = async (code: string) => {
+    const nextList = await getNextCode(code);
+    setSelected("dvsn", code);
+    setSectList(nextList);
+  };
+
+  const sectClick = async (code: string) => {
+    setSelected("sect", code);
+    setCode(code);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const cityResponse = await getNextCode("0000000000");
+        setCityList(cityResponse);
+      } catch (error) {
+        console.error("Error fetching region data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className={styles["filter-container"]}>
+      <div className={styles.title}>{title}</div>
+      <div className={styles["region-box"]}>
+        <div className={styles["region-container"]}>
+          {cityList.map((item) => (
+            <RegionItem
+              name={item.cortar_name}
+              code={item.cortar_no}
+              onClick={cityClick}
+              isClicked={selected.city === item.cortar_no}
+            />
+          ))}
+        </div>
+        <div className={styles["region-container"]}>
+          {dvsnList.map((item) => (
+            <RegionItem
+              name={item.cortar_name}
+              code={item.cortar_no}
+              onClick={dvsnClick}
+              isClicked={selected.dvsn === item.cortar_no}
+            />
+          ))}
+        </div>
+        <div className={styles["region-container"]}>
+          {sectList.map((item) => (
+            <RegionItem
+              name={item.cortar_name}
+              code={item.cortar_no}
+              onClick={sectClick}
+              isClicked={selected.sect === item.cortar_no}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const type = ["원룸", "빌라", "오피스텔", "아파트", "상가", "기타"];
-const transactionType = ["월세", "전세", "매매", "단기매매"];
+const transactionType = ["월세", "전세", "매매"];
 
 const FilterArea = ({ title, target }: { title: string; target: string[] }) => {
   const [isSelected, setIsSelected] = useState<string | undefined>();
@@ -130,9 +309,11 @@ export default function FilterPage() {
   return (
     <div className={styles.container}>
       <div />
-      <FilterArea title={"매물 종류"} target={type} />
+      <FilterRegion title={"지역"} />
       <div className={styles.divider} />
       <FilterArea title={"거래 방식"} target={transactionType} />
+      <div className={styles.divider} />
+      <FilterArea title={"매물 종류"} target={type} />
       <div className={styles.divider} />
       <FilterTargetWithSlider target={priceType} />
       <div className={styles.divider} />
